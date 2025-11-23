78969b1676c07eb20e2b9a583336aa62eb235c47
ABDULKADIR Ibrahim hassan <hassanabdulkadir480@gmail.com>
2025-11-23 07:05:51 +0100

    main.py

--- main.py
+++ main.py
@@ -66,6 +66,5 @@ def main():
         print("Visitor added successfully!")
     except Exception as e:
         print("Error:", e)
-
-if __name__ == "__main__":
+        if __name__ == "__main__":
     main()
